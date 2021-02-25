340e6e8863dd02807edbb3e3da6fcf89117e7352
ekonermann <ekonermann@luc.edu>
2021-02-25 01:03:22 +0000

    Progress on HW5. ERROR: conflicting types for putc. Seems to have been declared in stdio.h with a file parameter. Maybe dont call this function in kernel_main.c where i include stdio.h?

--- src/serial.c
+++ src/serial.c
@@ -1,3 +1,6 @@
-void putc(){
+#include "serial.h"
 
+void putc(int data){
+	char *mu_io = 0x3F215040;
+	*mu_io = data;
 }
---
+++ src/serial.h
@@ -0,0 +1,6 @@
+#ifndef _SERIAL_FUNC
+#define _SERIAL_FUNC
+
+void putc(int data);
+
+#endif
